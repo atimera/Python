3e04b0096e7365573d2cb2e57b4e48f205cef21e
laenzo <amdiatoutimera@hotmail.fr>
2017-08-05 20:37:42 +0200

    création et édition de dictionnaires, aussi fonctions dans les dico

--- dictionnaires.py
+++ dictionnaires.py
@@ -8,3 +8,25 @@ d["pseudo"] = "laenzo"
 d["password"] = "***"
 d[""]= "nom"
 print(d)
+
+inventaire = {
+        "pommes":45, "oranges":26, "mangues":32, "poires":21, "fraises":18
+    }
+print(inventaire)
+v = inventaire.pop("mangues") #ou bien: del inventaire["mangues"]
+print(inventaire)
+
+def aboie():
+    print("wof wof !!!")
+def miole():
+    print("miaaw miaaw !!")       
+def crie():
+    print("ahhhhhhh... !!")
+print2 = print
+print2("OK c'est bon")
+          
+dic = {"chat":miole, "chien":aboie, "enfant":crie}
+dic["chien"]()
+dic["enfant"]()
+dic["chat"]()
+
