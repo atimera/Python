adbef1742edf3f535a0494d2d2446841cff3c3ae
laenzo <amdiatoutimera@hotmail.fr>
2017-08-06 01:33:28 +0200

    Lecture et écriture dans un fichier

--- fichiers.py
+++ fichiers.py
@@ -1,3 +1,5 @@
+# -*-coding:Latin-1 -*
+
 import os
 
 # reportoire de travail
@@ -6,4 +8,21 @@ working_dir = os.getcwd()
 
 print(working_dir)
 #chdir("le rep de travail") pour changer de rep de travail
+
+# Lecture et écriture dans une fichier
+
+fichier = open("files/test.txt", "r") # r:read, w:write, a:append
+
+print(fichier.closed) # true if it's closed
+print("le fichier ouvert est: {}".format(fichier))
+
+contenu = fichier.read()
+lignes = contenu.split('\n')
+lignes = [line for line in lignes if line != ""] # sans les ligne vides
+print(lignes)
+fichier.close()
+
+with open("files/test1.txt", "w") as fichier: # fichier se close automaitquement
+    fichier.write("Je viens d'écrire ceci: \nHéllo tout le monde !!")
+
 
